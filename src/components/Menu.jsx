c1058d18b2b6a944052b6c02cce9fbefb25bd1b0
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-10 10:21:27 +0700

    done no chikd in collapse sidebar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,11 +8,27 @@ export default class Menu extends Component {
         <aside className="sidebar-mini main-sidebar  sidebar-dark-primary " style={{overflowX: 'hidden'}}>
           {/* Brand Logo */}
           <a href="#" class="brand-link logo-switch">
-            <img src="dist/img/rabbitcard-logo.jpg" alt="Rabbitcard Logo Small" class="brand-image-xl logo-xs" style={{top:"0", left: "0px", maxHeight: "55px", width: "65px", padding: "0px", transition:"10s"}} />
+            <img src="dist/img/rabbitcard-logo.jpg" alt="Rabbitcard Logo Small" class="brand-image-xl logo-xs" style={{top:"0", left: "0px", maxHeight: "55px", width: "60px", padding: "0px", transition:"10s"}} />
             <img src="dist/img/rabbitcard-logo.png" alt="Rabbitcard Logo Large" class="brand-image-xl logo-xl" style={{top:"0", left: "0px", maxHeight: "55px", width: "266px", padding: "0px"}} />
           </a>
           {/* Sidebar */}
-          <div className="sidebar">
+
+          <div class="form-inline mt-2" style={{padding:"0px 10px"}}>
+              <div class="input-group">
+                <input 
+                  class="form-control form-control-sidebar" 
+                  type="search" 
+                  placeholder="Search" 
+                  aria-label="Search" 
+                />
+                <div class="input-group-append">
+                  <button class="btn btn-sidebar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          <div className="sidebar" style={{padding: "0px 0px 0px 0px"}}>
             {/* Sidebar user panel (optional) */}
             {/* <div className="user-panel mt-3 pb-3 mb-3 d-flex">
               <div className="image">
@@ -29,24 +45,10 @@ export default class Menu extends Component {
               </div>
             </div> */}
             {/* Sidebar Search form */}
-            <div class="form-inline mt-2">
-              <div class="input-group">
-                <input 
-                  class="form-control form-control-sidebar" 
-                  type="search" 
-                  placeholder="Search" 
-                  aria-label="Search" 
-                />
-                <div class="input-group-append">
-                  <button class="btn btn-sidebar" type="submit">
-                    <i class="fas fa-search"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
+            
             <nav className="mt-2">
               <ul
-                className="nav nav-pills nav-sidebar flex-column"
+                className="nav nav-pills nav-sidebar flex-column nav-collapse-hide-child"
                 data-widget="treeview"
                 role="menu"
                 data-accordion="false"
