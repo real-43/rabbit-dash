c4242312d8690b02b48fd8b22c51b0b2260e54f9
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-10 14:36:23 +0700

    redirect to login first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import Header from './components/Header';
-import Menu from './components/Menu';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
+import ComDash from './Container/Fusion/fusion'
 import LoginPage from './Container/Login/login';
 import {Router,Route,Routes} from 'react-router-dom';
 
 function App() {
-  const DashWindow = 
-  <div class="wrapper">
-    <Header/>
-    <Menu/>
-    <Dashboard/>
-    <Footer/>
-  </div>
+  
   return (
-    
       <Routes>
-        <Route path="/" element={DashWindow}/>
+        <Route path="/" element={<ComDash />}/>
         <Route path='/login' element={<LoginPage />} />
       </Routes>
   );
---
+++ src/Container/Fusion/fusion.jsx
@@ -0,0 +1,32 @@
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { auth } from '../../firebase'
+import Header from '../../components/Header';
+import Menu from '../../components/Menu';
+import Dashboard from '../../components/Dashboard';
+import Footer from '../../components/Footer';
+
+export default function fusion() {
+
+    const router = useNavigate();
+    useEffect(() => {
+        const authentication = onAuthStateChanged(auth,(user) => {
+            if (user) {
+                router('/')
+            } else {
+                router('/login')
+           }
+        }) 
+        
+        return authentication
+    },[])
+    return (
+        <div class="wrapper">
+            <Header/>
+            <Menu/>
+            <Dashboard/>
+            <Footer/>
+        </div>
+  )
+}
