5112ee1dbf5e28548a75d03473fef74fbf86731e
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-10 14:06:44 +0700

    login done but no rule and did not show on first page

---
+++ src/Container/Login/login.jsx
@@ -0,0 +1,122 @@
+import React, { useState,useRef } from 'react'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router'
+import AlertBox from '../../components/alert';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth,provider } from '../../firebase';
+import './login.css'
+
+const theme = createTheme();
+export default function Login() {
+
+  const [userInfo, setUserInfo] = useState({ email: '', password: '' });
+  const [alert, setAlert] = useState({visible:false, severity:'', message:''});
+  const router = useNavigate();
+  const timerRef = useRef(null);
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+  signInWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+    .then(userInformation => {
+      console.log(userInformation);
+      router('/')
+    })
+    .catch(error => {
+    setAlert({ visible:true,severity:'error',message:error.message})
+        console.log(error.code);
+        timerRef.current= setTimeout(() => {
+          setAlert({ visible:false,severity:'',message:''})
+        },2000)
+  })
+};
+  
+  // const handlePassword = () => {
+  //   sendPasswordResetEmail(auth, userInfo.email)
+  //     .then(() => {
+  //       setAlert({ visible: true, severity: 'success', message: 'Email Sent Successfully' })
+  //        timerRef.current= setTimeout(() => {
+  //         setAlert({ visible:false,severity:'',message:''})
+  //       },2000)
+  //     })
+  //     .catch(error => {
+  //     setAlert({ visible:true,severity:'error',message:error.message})
+  //       console.log(error.code);
+  //       timerRef.current= setTimeout(() => {
+  //         setAlert({ visible:false,severity:'',message:''})
+  //       },2000)
+  //   })
+  // }
+    
+  const handleGoogleButton = () => {
+    signInWithPopup(auth, provider).then(result => {
+      console.log(result)
+      router('/')
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <AlertBox visible={alert.visible} severity={alert.severity} message={alert.message}/>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={userInfo.email}
+              onChange={(event)=>{setUserInfo({...userInfo,email:event.target.value})}}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={userInfo.password}
+              onChange={(event)=>{setUserInfo({...userInfo,password:event.target.value})}}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+          <button className='google_button' onClick={handleGoogleButton}><img src={"./google.png"} alt=''/></button>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+}
---
+++ src/firebase.jsx
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTDIyQunu1knIHSL9v85ER7mNAmM9EIeQ",
+    authDomain: "fir-49681.firebaseapp.com",
+    databaseURL: "https://fir-49681-default-rtdb.firebaseio.com",
+    projectId: "fir-49681",
+    storageBucket: "fir-49681.appspot.com",
+    messagingSenderId: "309579234663",
+    appId: "1:309579234663:web:b86c80d9515579277003c2",
+    measurementId: "G-448196V9C8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firebase Authentication object
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider()
+
+export { auth, provider }
