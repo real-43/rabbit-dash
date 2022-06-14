dd4cf47714991cc5995ef208b2b845ebee2e4a98
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-14 13:11:36 +0700

    forgotpaassword broken

--- src/Container/Signup/signup.jsx
+++ src/Container/Signup/signup.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {  createUserWithEmailAndPassword, sendEmailVerification, updateProfile, onAuthStateChanged, deleteUser, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword, sendEmailVerification, updateProfile, onAuthStateChanged, deleteUser, updatePassword, signInWithEmailAndPassword } from "firebase/auth";
 import { authSec, db } from '../../firebaseSec'
 import { auth } from '../../firebase'
 import { useNavigate } from 'react-router'
+import ReactModal from "react-modal";
 import {
   collection,
   getDocs,
@@ -11,15 +12,16 @@ import {
   deleteDoc,
   doc,
 } from "firebase/firestore";
-import './signup.css'
 
-const theme = createTheme();
-const user = auth.currentUser;
+// const theme = createTheme();
+// const user = auth.currentUser;
 
 export default function Signup() {
 
   const [userInfo, setUserInfo] = useState({ userName: '', email: '', password: '' });
   const [alert, setAlert] = useState({visible:false, severity:'', message:''});
+  const [isOpen, setIsOpen] = useState(false);
+  const [newPassword, setNewPassword] = useState(" ")
   const router = useNavigate();
   const timerRef = useRef(null);
 
@@ -71,13 +73,26 @@ export default function Signup() {
   },[])
   //On Click of submit button
 
-  const forgotPassword = (Email) => {
-  sendPasswordResetEmail(Email)
-      .then(function () {
-          alert('Please check your email...')
-      }).catch(function (e) {
-          console.log(e)
-      }) 
+  const forgotPassword = async (user) => {
+    await signInWithEmailAndPassword(authSec, user.email, user.password)
+    .then(() => {
+      updatePassword(authSec, newPassword)
+      authSec.signOut()
+    })
+    
+  }
+
+ 
+
+  function confirm(e) {
+    if (e) e.preventDefault();
+    console.log(" ");
+    dismiss();
+    setNewPassword("");
+  }
+
+  function dismiss() {
+    setIsOpen(false);
   }
 
   const handleSubmit = (event) => {
@@ -102,77 +117,84 @@ export default function Signup() {
         },2000)
     })
   };
-
-
    return (
     <div className="content-wrapper">
-      <h3>Management {'>'} User</h3>
-      <div className='input-wrapper'>
-        <h4>Create New User</h4>
-        <div className='input-container'>
-          <input className='input-register'
-            placeholder="Name..."
-            onChange={(event) => {
-              setUserInfo({ ...userInfo, userName: event.target.value })
-            }}
-          />
-          <input className='input-register'
-            type="Email"
-            placeholder="Email..."
-            onChange={(event) => {
-              setUserInfo({ ...userInfo, email: event.target.value })
-            }}
-          />
-          <input className='input-register'
-            type="Password"
-            placeholder="Password..."
-            onChange={(event) => {
-              setUserInfo({ ...userInfo, password: event.target.value })
-            }}
-          />
-          <button onClick={handleSubmit}> Create User</button>
-        </div>
-      </div>
-      <div className='table-container'>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Email</th>
-              <th>function</th>
-            </tr>
-          </thead>
-          {users.map((user) => {return (
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>{user.userName}</td>
-              <td>{user.email}</td>
-              <td className='btn-table'> 
-                <button
-                  className='del btn'
-                  onClick={() => {
-                    deleteUserOnFstored(user);
-                  }}
-                >
-                  {" "}
-                  Delete User
-                </button>
-                <button
-                  className='edit btn'
-                  onClick={() => {
-                    forgotPassword(user.email);
-                  }}
-                >
-                  {" "}
-                  Edit
-                </button>
-              </td>
-            </tr>
-          </tbody>)})}
-        </table>
-      </div>
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setUserInfo({ ...userInfo, userName: event.target.value })
+        }}
+      />
+      <input
+        type="Email"
+        placeholder="Email..."
+        onChange={(event) => {
+          setUserInfo({ ...userInfo, email: event.target.value })
+        }}
+      />
+      <input
+        type="Password"
+        placeholder="Password..."
+        onChange={(event) => {
+          setUserInfo({ ...userInfo, password: event.target.value })
+        }}
+      />
+      <button onClick={handleSubmit}> Create User</button>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Email</th>
+            <th>function</th>
+          </tr>
+        </thead>
+        {users.map((user) => {return (
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>{user.userName}</td>
+            <td>{user.email}</td>
+            <td> 
+              <button
+                onClick={() => {
+                  deleteUserOnFstored(user);
+                }}
+              >
+                {" "}
+                Delete User
+              </button>
+              <button
+                onClick={(e) => setIsOpen(true)}
+              >
+                {" "}
+                Edit
+              </button>
+              <ReactModal
+                appElement={document.getElementById("root")}
+                isOpen={isOpen}
+                onRequestClose={dismiss}
+                style={{
+                  overlay: { backgroundColor: "rgba(0,0, 0, 0.30" },
+                  content: { width: 400, height: 150 },
+                }}
+              >
+                <form onSubmit={confirm}>
+                  <label>
+                    Enter some text:
+                    <input
+                      type="text"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                  </label>
+                  <button type="submit" onClick={() =>{forgotPassword(user)}}>Confirm</button>
+                </form>
+              </ReactModal>
+            </td>
+          </tr>
+        </tbody>)})}
+      </table>
     </div>
   );
 }
