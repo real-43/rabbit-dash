757e60b12650851b3307e57147eed38136625960
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-16 16:34:49 +0700

    home datta monitor with mdb

--- src/Container/Projects/Maintenance Fee/Home.jsx
+++ src/Container/Projects/Maintenance Fee/Home.jsx
@@ -1,47 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { MDBDataTable } from 'mdbreact';
 
-export default function HealCheck() {
+const DatatablePage = () => {
   return (
-    <div className='content-wrapper'>
-        <h2>Mornitoring</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">SOURCE TYPE</th>
-                    <th scope="col">TRANSACTION DATE</th>
-                    <th scope="col">SETTLEMENT DATE</th>
-                    <th scope="col">TERMINAL ID</th>
-                    <th scope="col">VALUE</th>
-                    <th scope="col">TRANSACTION TYPE</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">LP</th>
-                    <td>20/02/2021 07:49:21</td>
-                    <td>unmatched</td>
-                    <td>@2021-05-27T11:20:27.467Z</td>
-                    <td>116.75</td>
-                    <td>Purse Use</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>BOBO</td>
-                    <td>bebe</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    <td>BOBO</td>
-                    <td>bebe</td>
-                </tr>
-            </tbody>
-        </table>    
+    <div className="content-wrapper">
+    <MDBDataTable
+      striped
+      bordered
+      small
+      data={data}
+    />
     </div>
-  )
+  );
 }
+
+export default DatatablePage;
