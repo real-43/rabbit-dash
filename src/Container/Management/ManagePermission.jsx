5d8dc7518a3867076a2ef11d4f9707f286138340
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-06-22 15:05:45 +0700

    Manage permission

--- src/Container/Management/ManagePermission.jsx
+++ src/Container/Management/ManagePermission.jsx
@@ -1,12 +1,129 @@
-import React, { useState } from 'react'
-import { getUsers } from '../../MyFireStore'
+import React, { Profiler, useEffect, useState } from 'react'
+import {Form, Button}  from 'react-bootstrap';
+import './ManagePermission.css'
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
 export default function ManagePermission() {
-    const [users, setUsers] = useState(null)
-    
-    // getUsers().then((value) => {setUsers(value)});
-    // console.log("IM USER: ", users)
+
+    const [projectInput, setProjectInput] = useState([]);
+    const [data, setdata] = useState(null);
+
+    const mockupRole = {
+        name: "staff", 
+        project: [
+            {
+                name: "Datafile", 
+                subMenu: ["Home", "PDF"]
+            },
+            {
+                name: "Maintenance Fee", 
+                subMenu: ["HealthCheck", "Logs"]
+            }
+        ]}
+
+    const mockupProject = [
+        {
+            name: "Datafile",
+            subMenu: ["Home", "PDF", "Setting", "Notification"]
+        },
+        {
+            name: "Maintenance Fee",
+            subMenu: ["HealthCheck", "Logs"]
+        }
+    ]
+
+    const optionsProject = () => {
+        var names = []
+        var index = 0
+        mockupProject.map((p) => {
+            names[index] = {value: p.name, label: p.name}
+            index = index + 1
+        })
+        console.log("input: ", projectInput)
+        return names
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const animatedComponents = makeAnimated();
+
+    const subMenuOptions = () => {
+        var filteredProject = [{name: "", options: [{value: "", label: ""}]}]
+        var index = 0
+        projectInput.map((inp) => {
+            
+            mockupProject.map((moc) => {
+                var option = []
+                var indexOption = 0
+                // if project in input
+                if (inp.value === moc.name) {
+                    moc.subMenu.map((sub) => {
+                        option[indexOption] = {value: sub, label: sub}
+                        indexOption = indexOption + 1
+                    })
+                    filteredProject[index] = {name: moc.name, options: option}
+                    index = index + 1
+                }
+            })
+        })
+        
+        setdata(filteredProject)
+        console.log("data", data, filteredProject)
+    }
+
     return (
-        <div className='content-wrapper'>ManagePermission</div>
+        <div className='content-wrapper'>
+            <div className='ManagePermission'>
+                <div className='create-permission'>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Permission Name</Form.Label>
+                            <Form.Control type="text" placeholder="Name" onChange={(e) => {}} />
+                        </Form.Group>
+                        
+                        <Form.Group className="mb-3">
+                            <Form.Label>Select Projects</Form.Label>
+                            <div className="mb-3">
+                                <Select
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    isMulti
+                                    options={optionsProject()}
+                                    onChange={(event) => {
+                                        setProjectInput(event)
+                                        subMenuOptions()
+                                    }}
+                                />
+                            </div>
+                        </Form.Group>
+                        {(data !== null) ? (
+                            data.map((project) => {
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Select Sub Menu of {project.name}</Form.Label>
+                                    <div className="mb-3">
+                                        <Select
+                                            closeMenuOnSelect={false}
+                                            components={animatedComponents}
+                                            isMulti
+                                            options={project.options}
+                                            onChange={(event) => {
+                                                
+                                            }}
+                                        />
+                                    </div>
+                                </Form.Group>
+                            })
+                        ) : ""}
+                       
+                        <Button onClick={(e) => handleSubmit()} variant="primary" type="submit">
+                            Create
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
     )
 }
