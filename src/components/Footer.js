7452d50631198a2ab6ca7318bae5d295df630674
real-43 <60128410+real-43@users.noreply.github.com>
2022-06-27 10:11:33 +0700

    layout background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,14 +13,14 @@ export default function Footer() {
   return (
     <div className="footer">
       <footer class="main-footer">
-        <div class="float-left d-none d-sm-inline-block">
+        <div class="float-left d-none d-sm-inline-block" style={{fontSize: "10px"}}>
           Copyright © 2014-2021 AdminLTE.io. All rights reserved.
         </div>
-        <strong className="float-center footer-content">
+        <strong className="float-center footer-content" style={{fontSize: "10px", textAlign: "center"}}>
           Today: {`${objDate.getDate()}/${objDate.getMonth()+1}/${objDate.getFullYear()}`} {objDate.toLocaleTimeString()}
           {callTime()}
         </strong>
-        <div class="float-right d-none d-sm-inline-block">
+        <div class="float-right d-none d-sm-inline-block" style={{fontSize: "10px"}}bb>
           <b>Version</b> 1.0.0
         </div>
       </footer>
