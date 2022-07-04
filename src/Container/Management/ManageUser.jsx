1481a7bf0aaea8c054c16f3f58e92d6e06c35777
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-07-04 12:06:14 +0700

    forgot to discard change

--- src/Container/Management/ManageUser.jsx
+++ src/Container/Management/ManageUser.jsx
@@ -43,7 +43,6 @@ export default function Signup() {
 
   const [users, setUsers] = useState([...usersR])
   const [roles, setRoles] = useState([...rolesR])
-  const [userCreate, setUserCreate] = useState(null)
 
   const updateData = async () => {
     await onSnapshot(collection(db,"roles"),(function(querySnapshot) {
@@ -211,27 +210,23 @@ export default function Signup() {
     setIsLoading(true);
 
     let uInfo = userInfo
-    let newUser = null
-    let newUID = null
 
-    // let rest = [...users]
-    // rest.push({
-    //   name: userInfo.name,
-    //   email: userInfo.email,
-    //   password: userInfo.password,
-    //   isBlocked: false,
-    //   role: ""
-    // })
-    // setUsers(rest)
+    let rest = [...users]
+    rest.push({
+      name: userInfo.name,
+      email: userInfo.email,
+      password: userInfo.password,
+      isBlocked: false,
+      role: ""
+    })
+    setUsers(rest)
 
     setUserInfo({ name: '', email: '', password: '' })
     
     // Create user in firebase auth
     await createUserWithEmailAndPassword(authSec, uInfo.email, uInfo.password)
       .then((userInformation) => {
-        newUser = authSec.currentUser
-        newUID = newUser.uid
-        updateProfile(newUser, {
+        updateProfile(authSec.currentUser, {
           displayName:userInformation.name
         })
       }).catch(error => {
@@ -242,8 +237,10 @@ export default function Signup() {
         },2000)
     })
 
+    setIsLoading(false);
+
     // To create user in firestore
-    await setDoc(doc(db, "users", newUID), {
+    await setDoc(doc(db, "users", authSec.currentUser.uid), {
       name: uInfo.name,
       email: uInfo.email,
       password: uInfo.password,
