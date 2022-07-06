6484ba9a94a66351110081a2b65ccb499782cf90
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-07-06 15:03:12 +0700

    Revert "Update gerate only showing data"
    This reverts commit 46253f042f100c800afb41842f952e65e3fdfdfb.

--- src/Container/Projects/Datafile Morniting/TableContainer.jsx
+++ src/Container/Projects/Datafile Morniting/TableContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import {
   useTable,
   useSortBy,
@@ -9,15 +9,8 @@ import {
 import { Table, Row, Col, Button, Input } from 'reactstrap';
 import { Filter, DefaultColumnFilter } from './filters';
 import './Table.css'
-import jsPDF from "jspdf";
-import "jspdf-autotable";
-import Loading from '../../../components/Loading';
-
 
 const TableContainer = ({ columns, data, renderRowSubComponent }) => {
-
-  const [isLoading, setIsLoading] = useState(false);
-
   const {
     getTableProps,
     getTableBodyProps,
@@ -55,140 +48,94 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
     setPageSize(Number(event.target.value));
   };
 
-  function exportPDF() {
-    setIsLoading(true)
-    console.log("loading", isLoading)
-    const unit = "pt";
-    const size = "A3"; // Use A1, A2, A3 or A4
-    const orientation = "portrait"; // portrait or landscape
-
-    const marginLeft = 40;
-    const doc = new jsPDF(orientation, unit, size);
-
-    doc.setFontSize(15);
-
-    const title = "Datafile Mornitoring";
-    const headers = [["ID", "DEVICE ID", "LOCATION ID", "SOURCE TYPE", "STATUS", "TXN DATE", "TXN TYPE", "VALUE", "CREATE DATA AT", "UPDATE DATA AT"]];
-
-    const info = []
-    page.map((p, index) => {
-      let arr = []
-      p.cells.map((cell) => {
-        arr.push(cell.value)
-      })
-      info.push(arr)
-    })
-
-    let content = {
-        startY: 50,
-        head: headers,
-        body: info
-    };
-
-    doc.text(title, marginLeft, 40);
-    doc.autoTable(content);
-    doc.save("report.pdf")
-    setIsLoading(false)
-    console.log("loading", isLoading)
-}
-
   return (
-    <div className='content-wrapper'>
-      <Loading isLoading={isLoading} />
-      <div className='pdf-page'>
-        <div className='pdf-table'>
-          <Fragment >
-              <Table bordered hover {...getTableProps()} >
-                  <thead className='header-table'>
-                      {headerGroups.map((headerGroup) => (
-                          <tr {...headerGroup.getHeaderGroupProps()}>
-                              {headerGroup.headers.map((column) => (
-                                  <th {...column.getHeaderProps()} style={{width: "fit-content"}}>
-                                      <div className='header-content' {...column.getSortByToggleProps()}>
-                                          {column.render('Header')}
-                                          {generateSortingIndicator(column)}
-                                      </div>
-                                      <Filter column={column} />
-                                  </th>
-                              ))}
-                          </tr>
-                      ))}
-                  </thead>
-                  <tbody {...getTableBodyProps()}>
-                      {page.map((row) => {
-                          prepareRow(row);
-                          return (
-                                  <Fragment key={row.getRowProps().key}>
-                                      <tr>
-                                          {row.cells.map((cell) => {
-                                              return (
-                                                  <td className='content-tabel' {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                              );
-                                          })}
-                                      </tr>
-                                  </Fragment>
-                              );
-                          })}
-                  </tbody>
-              </Table>
+    <Fragment >
+        <Table bordered hover {...getTableProps()} >
+            <thead className='header-table'>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()} style={{width: "fit-content"}}>
+                                <div className='header-content' {...column.getSortByToggleProps()}>
+                                    {column.render('Header')}
+                                    {generateSortingIndicator(column)}
+                                </div>
+                                <Filter column={column} />
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
 
-            <Row className='under-table'>
-              <Col md={3}>
-                <Button
-                  color='primary'
-                  onClick={() => gotoPage(0)}
-                  disabled={!canPreviousPage}
-                >
-                  {'<<'}
-                </Button>
-                <Button
-                  color='primary'
-                  onClick={previousPage}
-                  disabled={!canPreviousPage}
-                >
-                  {'<'}
-                </Button>
-              </Col>
-              <Col md={2} style={{ marginTop: 7 }}>
-                Page{' '}
-                <strong>
-                  {pageIndex + 1} of {pageOptions.length}
-                </strong>
-              </Col>
-              <Col md={2}>
-                <Input
-                  type='select'
-                  value={pageSize}
-                  onChange={onChangeInSelect}
-                >
-                  {'>'}
-                  {[10, 20, 30, 40, 50].map((pageSize) => (
-                    <option key={pageSize} value={pageSize}>
-                      Show {pageSize}
-                    </option>
-                  ))}
-                </Input>
-              </Col>
-              <Col md={3}>
-                <Button color='primary' onClick={nextPage} disabled={!canNextPage}>
-                  {'>'}
-                </Button>
-                <Button
-                  color='primary'
-                  onClick={() => gotoPage(pageCount - 1)}
-                  disabled={!canNextPage}
-                >
-                  {'>>'}
-                </Button>
-              </Col>
-            </Row>
-          </Fragment>
-        </div>
-        <div>
-          <button className="pdf-btn" onClick={(e) => exportPDF()}>Generate showing data to PDF</button>
-        </div>
-      </div>
-    </div>
+            <tbody {...getTableBodyProps()}>
+                {page.map((row) => {
+                    prepareRow(row);
+                    return (
+                            <Fragment key={row.getRowProps().key}>
+                                <tr>
+                                    {row.cells.map((cell) => {
+                                        return (
+                                            <td className='content-tabel' {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                        );
+                                    })}
+                                </tr>
+                            </Fragment>
+                        );
+                    })}
+            </tbody>
+        </Table>
+
+      <Row className='under-table'>
+        <Col md={3}>
+          <Button
+            color='primary'
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          >
+            {'<<'}
+          </Button>
+          <Button
+            color='primary'
+            onClick={previousPage}
+            disabled={!canPreviousPage}
+          >
+            {'<'}
+          </Button>
+        </Col>
+        <Col md={2} style={{ marginTop: 7 }}>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </Col>
+        <Col md={2}>
+          <Input
+            type='select'
+            value={pageSize}
+            onChange={onChangeInSelect}
+          >
+            {'>'}
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </Input>
+        </Col>
+        <Col md={3}>
+          <Button color='primary' onClick={nextPage} disabled={!canNextPage}>
+            {'>'}
+          </Button>
+          <Button
+            color='primary'
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {'>>'}
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>
   );
 };
 
