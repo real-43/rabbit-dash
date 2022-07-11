c33edbb3c740f53dd4fdf7a9fa5f4106ac4f76d2
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-07-11 16:15:41 +0700

    project test

--- Frontend/src/components/__tests__/ManageProject.test.jsx
+++ Frontend/src/components/__tests__/ManageProject.test.jsx
@@ -1,11 +1,58 @@
-import React  from 'react';
-import ManageProject from '../../Container/Management/ManageProject';
-import renderer from 'react-test-renderer';
+import { render } from "@testing-library/react"
+import '@testing-library/jest-dom'
+import React from 'react'
+import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "core-js/stable";
 
 
-test("Test Function popupDel",()=>{
-    let component = renderer.create(<ManageProject />).getInstance();
+import firebaseSlice from "../../Reducer/firebaseSlice";
 
-    let tree =  component.popupDel();
-    expected(tree).toBe(tree)
+import ManageProject from '../../Container/Management/ManageProject'
+
+describe("Test Function popupDel",()=>{
+    const persistConfig = {
+        key: 'firebase',
+        storage,
+      };
+    
+    const reducers = combineReducers({ firebase: firebaseSlice });
+    
+    const persistedReducer = persistReducer(persistConfig, reducers);
+
+    const mockStore = configureStore({
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+    });
+
+    test("Manage Project", () => {
+        const { getByTestId } = render(
+            <Provider store={mockStore}>
+                <BrowserRouter styles={{pointerEvents: 'none',cursor: 'none'}}>
+                    <Routes>
+                        <Route path='/' exact element={<ManageProject/>} />
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        )
+
+        expect(true);
+    })
 } )
