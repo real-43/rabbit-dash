77e36bdc33b0b80f3d05dfdd6dd1dfc6b480515a
real-43 <60128410+real-43@users.noreply.github.com>
2022-07-12 17:13:10 +0700

    pull instance from loading component to test

--- Frontend/src/components/__tests__/Loading.test.jsx
+++ Frontend/src/components/__tests__/Loading.test.jsx
@@ -5,8 +5,10 @@ import { create } from 'react-test-renderer'
 describe('Loading', () => {
     test("test render loading model" ,()=> {
         const test = create(<Loading  isloading={true}/>)
+        console.log(test)
         const testInstance = test.root;
         console.log(testInstance)
-        expect(testInstance.findByType(Loading))
+        
+        expect(testInstance.findByType(Loading).props.isLoading).toBeTruthy
     })
 })
