906c8216f1b3ca92804f78c5bf66e92e176adb99
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-07-12 12:44:30 +0700

    test mangeuser

--- Frontend/src/components/__tests__/ManageUser.test.js
+++ Frontend/src/components/__tests__/ManageUser.test.js
@@ -1,25 +1,31 @@
 import { render } from "@testing-library/react"
 import '@testing-library/jest-dom'
 import React from 'react'
-import Login from "../../Container/Login/login"
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "core-js/stable";
 import store from '../../store'
+import ManageUsers from '../../Container/Management/ManageUser'
 
-test("Login input is render", () => {
+describe("Management User", () => {
 
     const { getByTestId } = render(
         <Provider store={store}>
             <BrowserRouter styles={{pointerEvents: 'none',cursor: 'none'}}>
                 <Routes>
-                    <Route path='/' exact element={<Login/>} />
+                    <Route path='/' exact element={<ManageUsers/>} />
                 </Routes>
             </BrowserRouter>
         </Provider>
     )
-    const emailInput = getByTestId("emailInput")
-    const passInput = getByTestId("passInput")
-    expect(emailInput).toBeTruthy()
-    expect(passInput).toBeTruthy()
+
+    test("ManageUser Input Render", () => {
+        const nameInput = getByTestId("inputUserName")
+        const passInput = getByTestId("inputPassword")
+        const emailInput = getByTestId("inputEmail")
+        expect(emailInput).toBeTruthy()
+        expect(passInput).toBeTruthy()
+        expect(nameInput).toBeTruthy()
+    })
+    
 })
